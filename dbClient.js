74a38a3981f0fda47a228daaa709bfc390093d6f
Rebecca Acio-Adea <rebeccaacioadea@yahoo.com>
2021-06-08 13:53:55 +0100

    removing aws credential

--- dbClient.js
+++ dbClient.js
@@ -5,8 +5,6 @@ const {
   DynamoDBClient
 } = require('@aws-sdk/client-dynamodb')
 
-// let parms = { credentials: { secretAccessKey: 'o9jpo8OKqLkSwTmPX7gwNGyIGrt0bcNoyRWGrSpm',
-//   accessKeyId: 'AKIA6I4X2TNMZ5MSPI3R' }, region: 'eu-west-2' }
 
 let parms = process.env.awsCredentials
 
